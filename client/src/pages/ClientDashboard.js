91eac6e89a618691cc7305d2bd6f9eb1a19cf89e
Development Express <machineos@developmentexpress.in>
2026-04-22 08:46:47 +0530

    fix: CLIENT inside component

--- client/src/pages/ClientDashboard.js
+++ client/src/pages/ClientDashboard.js
@@ -47,6 +47,8 @@ const NAV_LABELS = {
 };
 
 const ClientDashboard = () => {
+  const CLIENT = JSON.parse(localStorage.getItem('machineos_user') || '{}');
+  const clientName = CLIENT.name || 'Client';
   const navigate = useNavigate();
   const { t } = useLanguage(); // eslint-disable-line
   const { isMobile, isTablet } = useWindowSize();
